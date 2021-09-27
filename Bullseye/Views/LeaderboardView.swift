ab9a1baca5d191e16a754cb24ce7e1a68317be05
Lisa Vo <lisa.vo97@gmail.com>
2021-09-26 23:02:34 -0700

    Add display and dismiss functionality to the Leaderboard view

--- Bullseye/Views/LeaderboardView.swift
+++ Bullseye/Views/LeaderboardView.swift
@@ -8,11 +8,13 @@
 import SwiftUI
 
 struct LeaderboardView: View {
+	@Binding var leaderboardIsShowing: Bool
+	
 	var body: some View {
 		ZStack {
 			Color("BackgroundColor").edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
 			VStack(spacing: 10) {
-				HeaderView()
+				HeaderView(leaderboardIsShowing: $leaderboardIsShowing)
 				LabelView()
 				RowView(index: 1, score: 10, date: Date())
 			}
@@ -46,6 +48,7 @@ struct RowView: View {
 }
 
 struct HeaderView: View {
+	@Binding var leaderboardIsShowing: Bool
 	@Environment(\.verticalSizeClass) var verticalSizeClass
 	@Environment(\.horizontalSizeClass) var horizontalSizeClass
 	
@@ -62,7 +65,9 @@ struct HeaderView: View {
 			}
 			HStack {
 				Spacer()
-				Button(action: {}) {
+				Button(action: {
+					leaderboardIsShowing = false
+				}) {
 					RoundedImageViewFilled(systemName: "xmark")
 						.padding(.trailing)
 				}
@@ -90,13 +95,14 @@ struct LabelView: View {
 }
 
 struct LeaderboardView_Previews: PreviewProvider {
+	static private var leaderboardIsShowing = Binding.constant(false)
+	
 	static var previews: some View {
-		LeaderboardView()
-		LeaderboardView()
-			 .previewLayout(.fixed(width: 568, height: 320))
-		LeaderboardView()
+		LeaderboardView(leaderboardIsShowing: leaderboardIsShowing)
+		LeaderboardView(leaderboardIsShowing: leaderboardIsShowing)		 .previewLayout(.fixed(width: 568, height: 320))
+		LeaderboardView(leaderboardIsShowing: leaderboardIsShowing)
 			 .preferredColorScheme(.dark)
-		LeaderboardView()
+		LeaderboardView(leaderboardIsShowing: leaderboardIsShowing)
 			 .preferredColorScheme(.dark)
 			 .previewLayout(.fixed(width: 568, height: 320))
 	}
